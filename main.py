9a3f93c24de6b20ef1619afbebb3ff5ef5031bdb
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-28 12:45:31 -0500

    enum and file paths

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
-@app.get("/orders/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value is ModelName.lenet:
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
 
 
-# This needs to be after /orders/me route
-@app.get("/orders/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
