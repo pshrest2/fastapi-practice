c6b7d81bf64f6a3d7e98eac659adea7b340471fd
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-28 12:53:24 -0500

    optional parameters

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = FastAPI()
 items = [{"name": "Foo"}, {"name": "Bar"}, {"name": "Baz"}]
 
 
-@app.get("/items")
-async def read_items(skip: int = 0, limit: int = 0):
-    return items[skip : skip + limit]
+@app.get("/items/{item_id}")
+async def read_items(item_id: int, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
