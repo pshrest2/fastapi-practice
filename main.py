bd4adb894667d48ad9f2bd96e55db7af7e38f3e9
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-28 12:38:42 -0500

    order matters

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
+@app.get("/orders/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+# This needs to be after /orders/me route
+@app.get("/orders/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
