4c6c5a7cf6e0e4d7305d0252dba492703b618198
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-28 12:51:47 -0500

    using query params

--- main.py
+++ main.py
@@ -3,24 +3,9 @@ from enum import Enum
 
 app = FastAPI()
 
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+items = [{"name": "Foo"}, {"name": "Bar"}, {"name": "Baz"}]
 
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value is ModelName.lenet:
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get("/items")
+async def read_items(skip: int = 0, limit: int = 0):
+    return items[skip : skip + limit]
