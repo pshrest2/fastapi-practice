cadf36c9095685263bd3c5c1db56cf4062c18083
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-28 13:51:38 -0500

    dependency injection in fastapi

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from typing import Annotated
 from pydantic import BaseModel
 
 app = FastAPI()
+
+items_db = [{"name": "Foo"}, {"name": "Bar"}, {"name": "Baz"}]
 
 
 class Item(BaseModel):
@@ -9,6 +12,23 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
+
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items")
+async def get_items(commons: Annotated[CommonQueryParams, Depends(CommonQueryParams)]):
+    res = {}
+    if commons.q:
+        res.update({"q": commons.q})
+    items = items_db[commons.skip : commons.skip + commons.limit]
+    res.update({"items": items})
+    return res
 
 
 @app.post("/items")
