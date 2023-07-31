9efa963328b267054af45819200e212455e5248a
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-31 14:35:18 -0500

    fix a few things

--- app/models/item.py
+++ app/models/item.py
@@ -5,8 +5,6 @@ from ..db.base_class import Base
 
 
 class Item(Base):
-    __tablename__ = "items"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
